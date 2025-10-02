348ac21b1fbc8ddc825fc63f2e4a8c6943c9c25f
jang-jinuk <dlsndifn98@gmail.com>
2025-10-02 11:53:36 +0900

    feat: MBR 박스를 생성 메서드 추가
    - 기준점으로부터 5km 반경의 MBR(Minimum Bounding Rectangle) 박스를 생성

--- src/main/java/com/dmz/airdnd/accommodation/util/GeoFactory.java
+++ src/main/java/com/dmz/airdnd/accommodation/util/GeoFactory.java
@@ -2,6 +2,7 @@ package com.dmz.airdnd.accommodation.util;
 
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.Polygon;
 
 public class GeoFactory {
 
@@ -14,5 +15,28 @@ public class GeoFactory {
 	// latitude : 위도
 	public static Point createPoint(double longitude, double latitude) {
 		return geometryFactory.createPoint(new Coordinate(longitude, latitude));
+	}
+
+	public static Polygon createBoundingBox(double longitude, double latitude) {
+		double radiusKm = 5.0;
+
+		// 위도 1도당 약 111km
+		double lonDiff = radiusKm / (111.045 * Math.cos(Math.toRadians(longitude)));
+		double latDiff = radiusKm / 111.045;
+
+		double minLon = longitude - latDiff;
+		double maxLon = longitude + latDiff;
+		double minLat = latitude - lonDiff;
+		double maxLat = latitude + lonDiff;
+
+		Coordinate[] coordinates = new Coordinate[] {
+			new Coordinate(minLon, minLat),
+			new Coordinate(maxLon, minLat),
+			new Coordinate(maxLon, maxLat),
+			new Coordinate(minLon, maxLat),
+			new Coordinate(minLon, minLat)
+		};
+
+		return geometryFactory.createPolygon(coordinates);
 	}
 }
